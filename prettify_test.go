33f963ed69307e6639b5b8cc7c8263a549351063
leon <leon@mobvista.com>
2019-07-17 15:38:16 +0800

    add pretty

---
+++ prettify_test.go
@@ -0,0 +1,24 @@
+package gokit
+
+import (
+	"testing"
+)
+
+func TestPrettify(t *testing.T) {
+	type Person struct {
+		Age    int
+		Name   *string
+		School map[string]*string
+	}
+
+	school := make(map[string]*string)
+	school["junior"] = String("junior school")
+	school["high"] = String("high school")
+	person := Person{
+		Age:    1,
+		Name:   String("jess"),
+		School: school,
+	}
+
+	t.Log(Prettify(person))
+}
