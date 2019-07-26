38dea63c27eb4d08d6e1368cf33994615722f9ba
leon <leon@mobvista.com>
2019-07-26 14:07:32 +0800

    add prettify json

--- prettify.go
+++ prettify.go
@@ -2,6 +2,7 @@ package gokit
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"reflect"
@@ -110,4 +111,16 @@ func prettify(v reflect.Value, indent int, buf *bytes.Buffer) {
 		}
 		_, _ = fmt.Fprintf(buf, format, v.Interface())
 	}
+}
+
+// Only for normal logging purpose, 4 space indent
+func PrettifyJson(i interface{}, indent bool) string {
+	var str []byte
+	if indent {
+		str, _ = json.MarshalIndent(i, "", "    ")
+	} else {
+		str, _ = json.Marshal(i)
+	}
+
+	return string(str)
 }
--- prettify_test.go
+++ prettify_test.go
@@ -4,13 +4,14 @@ import (
 	"testing"
 )
 
-func TestPrettify(t *testing.T) {
-	type Person struct {
-		Age    int
-		Name   *string
-		School map[string]*string
-	}
+type Person struct {
+	Name   *string
+	Age    int
+	School map[string]*string
+	Hobbit []string
+}
 
+func TestPrettify(t *testing.T) {
 	school := make(map[string]*string)
 	school["junior"] = String("junior school")
 	school["high"] = String("high school")
@@ -18,7 +19,23 @@ func TestPrettify(t *testing.T) {
 		Age:    1,
 		Name:   String("jess"),
 		School: school,
+		Hobbit: []string{"swim", "game"},
 	}
 
 	t.Log(Prettify(person))
+}
+
+func TestPrettifyJson(t *testing.T) {
+	school := make(map[string]*string)
+	school["junior"] = String("junior school")
+	school["high"] = String("high school")
+	person := Person{
+		Age:    1,
+		Name:   String("jess"),
+		School: school,
+		Hobbit: []string{"swim", "game"},
+	}
+	t.Log(PrettifyJson(person, false))
+
+	t.Log(PrettifyJson([]*string{String("book"), String("bed")}, true))
 }
