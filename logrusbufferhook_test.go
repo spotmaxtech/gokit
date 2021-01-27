94845f0ae3e5b2e52f6877180671148405cec1e1
Liuzoxan <liuzoxan@gmail.com>
2021-01-27 16:21:28 +0800

    update

--- logrusbufferhook_test.go
+++ logrusbufferhook_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	. "github.com/smartystreets/goconvey/convey"
+	"io/ioutil"
 	"testing"
+	"time"
 )
 
 func TestLogrusBufferHook(t *testing.T) {
@@ -33,5 +35,40 @@ func TestLogrusBufferHook(t *testing.T) {
 		for _, l := range logs {
 			fmt.Println(l)
 		}
+	})
+}
+
+func TestLogrusBufferHook_ThreadSafe(t *testing.T) {
+	Convey("logrus buffer hook", t, func() {
+		hook, buffer := NewLogrusBufferHook([]logrus.Level{logrus.WarnLevel, logrus.InfoLevel}, 10)
+
+		logrus.AddHook(hook)
+		logrus.SetOutput(ioutil.Discard)
+
+		go func() {
+			for i := 0; i < 10000; i++ {
+				logrus.Infof("Hello world! From-1 %d", i)
+				time.Sleep(time.Millisecond * 50)
+			}
+		}()
+
+		go func() {
+			for i := 0; i < 10000; i++ {
+				logrus.Infof("Hello world! From-2 %d", i)
+				time.Sleep(time.Millisecond * 100)
+			}
+		}()
+
+		go func() {
+			for i := 0; i < 20; i++ {
+				logs := buffer.Read()
+				for _, l := range logs {
+					fmt.Println(l)
+				}
+				time.Sleep(time.Second)
+			}
+		}()
+
+		time.Sleep(time.Second * 30)
 	})
 }
